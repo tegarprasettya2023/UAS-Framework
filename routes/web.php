06bc1dbd74e94748c163bf3d43b4c2b768f61e67
tegarprasettya2023 <tegarprasettya73@gmail.com>
2023-07-20 18:28:27 +0700

    index page home shop contact

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [Controller::class, 'index'])->name('home');
+Route::get('/shop', [Controller::class, 'shop'])->name('shop');
+Route::get('/transaksi', [Controller::class, 'transaksi'])->name('transaksi');
+Route::get('/contact', [Controller::class, 'contact'])->name('contact');
+Route::get('/checkout', [Controller::class, 'checkout'])->name('checkout');
